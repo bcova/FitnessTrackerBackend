b57ef0b5f91be6331dd80cd91755c3cd90fd4ad1
bcova <brandoncova@gmail.com>
2022-07-26 15:46:59 -0500

    Finishing up tests

--- db/users.js
+++ db/users.js
@@ -1,20 +1,21 @@
+/* eslint-disable no-useless-catch */
 const client = require("./client");
 const bcrypt = require('bcrypt');
 
 // database functions
 
 // user functions
-async function createUser({ username, password }) {
+async function createUser({ username, password}) {
   
   const SALT_COUNT = 10;
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
-  
+
   try {
     const { rows: [ user ] } = await client.query(`
     INSERT INTO users (username, password)
     VALUES ($1, $2)
     RETURNING username;`
-    , [username, password])
+    , [username, hashedPassword])
 
     return user
   } catch (error) {
@@ -24,20 +25,27 @@ async function createUser({ username, password }) {
 }
 
 async function getUser({ username, password }) {
-  const user = await getUserByUserName(username);
+  const user = await getUserByUsername(username);
   const hashedPassword = user.password;
   const isValid = await bcrypt.compare(password, hashedPassword)
+
+if(isValid){
+   delete user.password
+  return user  
+}
+
 }
 
 
 async function getUserById(userId) {
+
   try{
-  const { rows:[user] } = await client.query(`
+  const { rows:[ user ] } = await client.query(`
   SELECT id, username
   FROM users
-  WHERE id=${userId};
+  WHERE id=${ userId };
 `);
- 
+
  return user;
  
   }catch (error){
